ae674a0b8d842befa1d92da5814497b8ce623621
Chris Purnell <cooksystems@Cooks-MacBook-Pro.local>
2022-05-16 15:58:05 -0500

    added legend on loadout

--- igl/src/components/LoadoutGrid.js
+++ igl/src/components/LoadoutGrid.js
@@ -1,4 +1,5 @@
 import Havoc from "../images/weapons/Havoc.png";
+import Loba from "../images/legends/loba.png";
 
 const LoadoutGrid = (props) => {
   const { loadouts } = props;
@@ -13,54 +14,62 @@ const LoadoutGrid = (props) => {
           </h2>
         </div>
         <div class="container mx-auto">
-        <div class="grid grid-cols-1 mt-8 lg:grid-cols-3 gap-x-4 gap-y-8">
-          {loadouts.map((currentLoadout, index) => (
-            <a href="" class="block border-2 border-sky-500 p-6 rounded-lg shadow-xl bg-white" key={index}>
-              <div class="flex justify-center">
-                <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-apexred">
-                  {" "}
-                  {currentLoadout.username}
-                </strong>
-              </div>
-            <div class="flex justify-center mt-6 p-6 rounded-lg gap-x-6">
-              <img
-                alt="Trainer Product"
-                src={Havoc}
-                class="w-32 h-24 -mt-3 p-6 border-b-4 border-green"
-              />
+          <div class="grid grid-cols-1 mt-8 lg:grid-cols-3 gap-x-4 gap-y-8 relative">
+            {loadouts.map((currentLoadout, index) => (
+              <a
+                href=""
+                class="block border-2 border-sky-500 p-6 rounded-lg shadow-xl bg-white"
+                key={index}
+              >
+                <img
+                  class="absolute h-20 w-20 rounded-full -top-10 -left-10 border-2 border-apexred m-4 p-1 bg-white"
+                  src={Loba}
+                />
+                <div class="flex justify-center">
+                  <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-apexred">
+                    {" "}
+                    {currentLoadout.username}
+                  </strong>
+                </div>
+                <div class="flex justify-center mt-6 p-6 rounded-lg gap-x-6">
+                  <img
+                    alt="Trainer Product"
+                    src={Havoc}
+                    class="w-32 h-24 -mt-3 p-6 border-b-4 border-green"
+                  />
 
-              <img
-                alt="Trainer Product"
-                src={Havoc}
-                class="w-32 h-24 -mt-3 p-6 border-b-4 border-apexred"
-              />
-            </div>
+                  <img
+                    alt="Trainer Product"
+                    src={Havoc}
+                    class="w-32 h-24 -mt-3 p-6 border-b-4 border-apexred"
+                  />
+                </div>
 
-              <div class="flex items-center justify-between mt-4 font-bold flex-col">
-                <strong class="inline-flex items-center border border-gray-200 rounded relative px-2.5 py-1.5 text-xs font-medium m-auto mt-3 h-12">
-                  <span class="animate-ping w-2.5 h-2.5 bg-green rounded-full absolute -top-1 -left-1"></span>
-                  <span class="w-2.5 h-2.5 bg-green rounded-full absolute -top-1 -left-1"></span>
+                <div class="flex items-center justify-between mt-4 font-bold flex-col">
+                  <strong class="inline-flex items-center border border-gray-200 rounded relative px-2.5 py-1.5 text-xs font-medium m-auto mt-3 h-12">
+                    <span class="animate-ping w-2.5 h-2.5 bg-green rounded-full absolute -top-1 -left-1"></span>
+                    <span class="w-2.5 h-2.5 bg-green rounded-full absolute -top-1 -left-1"></span>
 
-                  <span class="text-gray"> Primary: </span>
+                    <span class="text-gray"> Primary: </span>
 
-                  <span class="text-green ml-1.5">
-                    {currentLoadout.primary}
-                  </span>
-                </strong>
+                    <span class="text-green ml-1.5">
+                      {currentLoadout.primary}
+                    </span>
+                  </strong>
 
-                <strong class="inline-flex items-center border border-gray-200 rounded relative px-2.5 py-1.5 text-xs font-medium m-auto mt-3 h-12">
-                  <span class="animate-ping w-2.5 h-2.5 bg-apexred rounded-full absolute -top-1 -left-1"></span>
-                  <span class="w-2.5 h-2.5 bg-apexred rounded-full absolute -top-1 -left-1"></span>
+                  <strong class="inline-flex items-center border border-gray-200 rounded relative px-2.5 py-1.5 text-xs font-medium m-auto mt-3 h-12">
+                    <span class="animate-ping w-2.5 h-2.5 bg-apexred rounded-full absolute -top-1 -left-1"></span>
+                    <span class="w-2.5 h-2.5 bg-apexred rounded-full absolute -top-1 -left-1"></span>
 
-                  <span class="text-gray-700"> Secondary: </span>
+                    <span class="text-gray-700"> Secondary: </span>
 
-                  <span class="text-apexred ml-1.5">
-                    {currentLoadout.secondary}
-                  </span>
-                </strong>
-              </div>
-            </a>
-          ))}
+                    <span class="text-apexred ml-1.5">
+                      {currentLoadout.secondary}
+                    </span>
+                  </strong>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
       </div>
